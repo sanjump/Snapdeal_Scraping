94afa15db8c411d3e139dfcb1805feadbbe7717c
Sanju <mpsanju123@gmail.com>
2020-08-25 17:20:23 +0530

    First Commit

---
+++ Snapdeal_Electronics/Snapdeal_Electronics/items.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class SnapdealElectronicsItem(scrapy.Item):
+    # define the fields for your item here like:
+    stores = scrapy.Field()
+    storelink = scrapy.Field()
+    category = scrapy.Field()
+    subcategory = scrapy.Field()
+    product_name = scrapy.Field()
+    storeprice = scrapy.Field()
+    description = scrapy.Field()
+    photos = scrapy.Field()
+    storeproductid = scrapy.Field()
+    product_id = scrapy.Field()
+    rating = scrapy.Field()
+    reviews = scrapy.Field()
+    spec_title = scrapy.Field()
+    spec_detail = scrapy.Field()
+    page = scrapy.Field()
---
+++ Snapdeal_Electronics/Snapdeal_Electronics/spiders/camera.py
@@ -0,0 +1,75 @@
+import scrapy,random,string
+from ..items import SnapdealElectronicsItem
+
+class SnapdealSpider(scrapy.Spider):
+
+    name = 'snapcamera'
+
+
+    start_urls=['https://www.snapdeal.com/products/cameras-digital-slrs?sort=plrty#bcrumbLabelId:290']
+
+    def parse(self, response):
+
+
+        page = response.css("a.dp-widget-link::attr('href')").getall()
+
+
+
+        for p in page[4:]:
+
+             yield scrapy.Request(p, callback=self.parse_elec)
+
+
+
+    def parse_elec(self, response):
+
+        items = SnapdealElectronicsItem()
+
+        product_name = response.css('.pdp-e-i-head::text').get()
+
+        storeprice = response.css('.payBlkBig::text').get()
+
+        storeLink = response.url
+
+        id = ''
+        k = storeLink.find('#')
+        while storeLink[k] != '/':
+            id = id + storeLink[k]
+            k -= 1
+
+        photos = response.xpath('//*[@id="bx-slider-left-image-panel"]/li[1]/img').xpath("@src").get()
+
+        spec_title = response.css(".product-spec td:nth-child(1)::text").extract()
+
+        spec_detail = response.css("td+ td::text").extract()
+
+        product_id = ''.join(random.sample(string.ascii_lowercase + string.digits, 20))
+
+        rating = response.css('span.avrg-rating::text').get()
+        reviews = response.css('#defaultReviewsCard p::text').extract()
+
+        stores = {
+
+            "rating": "0" if rating == None else rating[1:4],
+            "reviews":reviews,
+            'storeproductid': id[::-1],
+            "storeLink": storeLink,
+            "storeName": "Snapdeal",
+            "storePrice": storeprice,
+
+        }
+
+        items['product_name'] = product_name.strip()
+        items['product_id'] = product_id
+        items['stores'] = stores
+        items['category'] = 'electronics'
+        items['subcategory'] = 'camera'
+
+        items['description'] = {}
+
+        for i in range(len(spec_title)):
+            items['description'][spec_title[i]] = spec_detail[i]
+
+        items['photos'] = photos
+
+        yield items
